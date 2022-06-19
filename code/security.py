dd0e153e251c713de78b88b809f160deb2f64dc1
turtlez23 <mil.mazur@gmail.com>
2022-06-19 16:22:00 +0200

    Initial commit. Copy files from rest_api-flask-course1

---
+++ code/security.py
@@ -0,0 +1,12 @@
+import hmac
+from user import User
+
+def authenticate(username, password):
+  user = User.find_by_username(username)
+  if user and hmac.compare_digest(user.password, password):
+    # return {'id': user.id, 'username': user.username}
+    return user
+
+def identity(payload):
+  user_id = payload['identity']
+  return User.find_by_id(user_id)
