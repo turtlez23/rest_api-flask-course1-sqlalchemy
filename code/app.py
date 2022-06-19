d0d36f187c6e39e903ccbab1c778b9d82ff9675f
turtlez23 <mil.mazur@gmail.com>
2022-06-19 23:15:43 +0200

    Added stores repository

--- code/app.py
+++ code/app.py
@@ -5,6 +5,7 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 from db import db
 
@@ -12,14 +13,19 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = '123eefdfds2@'
-
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+  db.create_all()
 
 jwt = JWT(app, authenticate, identity) # /auth
 
-api.add_resource(ItemList, '/items')
 api.add_resource(Item, '/items/<string:name>')
+api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/stores/<string:name>')
+api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
   db.init_app(app)
--- code/resources/item.py
+++ code/resources/item.py
@@ -11,6 +11,11 @@ class Item(Resource):
     required=True,
     help="This field cannot be left blank!"
   )
+  parser.add_argument('store_id',
+    type=int,
+    required=True,
+    help="Every item needs a store id."
+  )
 
   @jwt_required()
   def get(self, name):
@@ -24,7 +29,7 @@ class Item(Resource):
       return {'message': f"An item with name {name} already exists."}, 400
 
     data = Item.parser.parse_args()
-    item = ItemModel(name, data['price'])    
+    item = ItemModel(name, **data)    
 
     try:
       item.save_to_db()
@@ -46,7 +51,7 @@ class Item(Resource):
 
     item = ItemModel.find_by_name(name)
     if item is None:
-      item = ItemModel(name, data['price'])
+      item = ItemModel(name, **data)
     else:
       item.price = data['price']
     item.save_to_db()
